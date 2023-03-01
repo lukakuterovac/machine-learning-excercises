9168eb94c234961eabad1f03ef8bab20799a6c6d
Luka Kuterovac <luka.kuterovac@gmail.com>
2023-03-01 13:58:02 +0100

    Complete task 3

---
+++ exercises-01/task-03/main.py
@@ -0,0 +1,18 @@
+def avg(arr):
+    return sum(arr) / len(arr)
+
+
+arr = []
+
+while True:
+    x = input()
+    if x.isnumeric():
+        arr.append(int(x))
+    elif x.lower() == "done":
+        break
+
+print(f"Min: {min(arr)}")
+print(f"Max: {max(arr)}")
+print(f"Avg: {avg(arr)}")
+arr.sort()
+print(f"Sort: {arr}")
